c4ff925109ced6ae480b7115c7b682d1b77cc7e6
Francis Padero <francis.padero@1.ustp.edu.ph>
2026-02-04 00:48:56 +0800

    chore: Initial requirements page and added some components in shadcn
    - temporary almost 1:1 copy on the  original page
    - initialize tags and layout
    - will modify the UI later after figma is done
    
    ShadCN
    
    - badge
    - card
    - input

---
+++ src/app/requirements/testing-page/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Testing = () => {
+  return (
+    <div className="flex flex-row min-h-screen justify-center bg-zinc-50 font-sans dark:bg-black overflow-auto p-8">
+        <h1>Testing Page</h1>
+        <p>This is the testing requirements page.</p>
+        <text>Test Pages for fun???</text>
+    </div>
+  )
+}
+
+export default Testing
