210702aaa1b6801b0bd47a04a4b8a6823b3ccd76
Francis Padero <francis.padero@1.ustp.edu.ph>
2026-02-03 23:20:54 +0800

    chore: initialize requirements page

---
+++ src/app/requirements/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Requirements = () => {
+  return (
+    <div>
+      <h1>This is the requirements page</h1>
+      <p>This is the main landing page of requirements page</p>
+    </div>
+  );
+};
+
+export default Requirements;
