f571aaea3695cfc1f967c7e773f3cb87fae879be
Francis Padero <francis.padero@1.ustp.edu.ph>
2026-02-03 23:41:05 +0800

    chore: test requirements branch commit
    - test smol changes

--- src/app/requirements/page.tsx
+++ src/app/requirements/page.tsx
@@ -5,6 +5,9 @@ const Requirements = () => {
     <div>
       <h1>This is the requirements page</h1>
       <p>This is the main landing page of requirements page</p>
+      <text>
+        Test changes on this file for this branch test TEST
+      </text>
     </div>
   );
 };
